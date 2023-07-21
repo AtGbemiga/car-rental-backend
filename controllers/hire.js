8570153d3e41d84a20fdd910d1a7b27fa220c665
Gbemiga Atolagbe <gbatolagbe@gmail.com>
2023-07-21 22:30:28 +0100

    th

--- controllers/hire.js
+++ controllers/hire.js
@@ -1,3 +1,4 @@
+// backend/controllers/hire.js
 const Vehicle = require("../models/Hire");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
@@ -26,21 +27,62 @@ const upload = multer({ storage: storage, fileFilter: fileFilter });
 const AddHire = async (req, res) => {
   console.log(req.body);
   upload.array("pictures", 5)(req, res, async (error) => {
-    // Rest of the multer upload handling code...
+    if (error) {
+      // Handle multer upload error
+      if (error.message === "Unexpected field") {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ error: "Maximum of 5 images allowed" });
+      }
 
-    // Extract the modified vehicle details from the request body
-    const { vehicle } = req.body;
-    const { note, ...vehicleDetails } = vehicle;
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
+    }
 
-    // Create a new vehicle entry with the modified details
-    const modifiedVehicle = await Vehicle.create({
-      ...vehicleDetails,
-      pictures: pictureUrls,
-      createdBy: req.user.userId,
-      note: note,
-    });
+    if (!req.files || req.files.length === 0) {
+      // No files uploaded
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "No files uploaded" });
+    }
 
-    res.status(StatusCodes.CREATED).json({ vehicle: modifiedVehicle });
+    if (req.files.length > 5) {
+      // More than 5 files uploaded
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: "Maximum of 5 images allowed" });
+    }
+
+    const pictureUrls = [];
+
+    // Upload each picture to Cloudinary and store the secure URLs
+    for (const file of req.files) {
+      const uniqueIdentifier =
+        Date.now() + "-" + Math.round(Math.random() * 1e9);
+      const publicId = `${req.user.userId}_vehicle_${uniqueIdentifier}`;
+
+      const result = await cloudinary.uploader.upload(file.path, {
+        public_id: publicId,
+        width: 500,
+        height: 500,
+        crop: "fill",
+      });
+
+      pictureUrls.push(result.secure_url);
+    }
+
+    req.body.pictures = pictureUrls;
+    req.body.createdBy = req.user.userId;
+
+    // Extract the note from the request body
+    const { note, ...vehicleDetails } = req.body;
+
+    // Create a new vehicle entry with the user's note
+    const vehicleWithNote = { ...vehicleDetails, note };
+
+    // Store the new vehicle entry in the database
+    const vehicle = await Vehicle.create(vehicleWithNote);
+
+    res.status(StatusCodes.CREATED).json({ vehicle });
   });
 };
 
